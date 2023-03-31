7c6d0f3b3f467c1137e83c2e6551df6ccb4b7a57
Mopano <99732517+funnypar@users.noreply.github.com>
2023-03-31 12:09:54 -0700

    Better way

--- 005/005.js
+++ 005/005.js
@@ -1,19 +1,22 @@
 // Create simple API and send JSON data to browser !!!
+// Doing that in better way !!! (Don't read file each time )
 
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
+const datas = fs.readFileSync(`${__dirname}/datas.json`, "utf-8");
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
     if (pathName === "/" || pathName === "/api") {
-        fs.readFile(`${__dirname}/datas.json`, "utf-8", (err, datas) => {
-            if (err) console.log(`Error !!!  ==>  ${err}`);
-            const bookDatas = JSON.parse(datas);
-            res.writeHead(200, { "content-type": "application/json" });
-            res.end(datas);
-        });
+        // fs.readFile(`${__dirname}/datas.json`, "utf-8", (err, datas) => {
+        //     if (err) console.log(`Error !!!  ==>  ${err}`);
+        // const bookDatas = JSON.parse(datas);
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(datas);
+        // });
     } else {
         res.writeHead(404, {
             "content-type": "text/html",
