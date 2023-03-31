92a31051d34a396f852be16f471a22d70b74cdf0
Mopano <99732517+funnypar@users.noreply.github.com>
2023-03-31 12:05:32 -0700

    Create simple API

---
+++ 005/005.js
@@ -0,0 +1,27 @@
+// Create simple API and send JSON data to browser !!!
+
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if (pathName === "/" || pathName === "/api") {
+        fs.readFile(`${__dirname}/datas.json`, "utf-8", (err, datas) => {
+            if (err) console.log(`Error !!!  ==>  ${err}`);
+            const bookDatas = JSON.parse(datas);
+            res.writeHead(200, { "content-type": "application/json" });
+            res.end(datas);
+        });
+    } else {
+        res.writeHead(404, {
+            "content-type": "text/html",
+        });
+        res.end("<h1>Chera nemiri ba dostat bashiiii </h1>");
+    }
+});
+
+server.listen("8000", "127.0.0.1", () => {
+    console.log("Run");
+});
