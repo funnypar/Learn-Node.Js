4243dbd7d31346955188d36b7a62ebcb41a909ce
Mopano <99732517+funnypar@users.noreply.github.com>
2023-04-23 02:00:00 -0700

    addedAuthor

--- 011-authors/app.js
+++ 011-authors/app.js
@@ -5,19 +5,38 @@ const fs = require('fs');
 const port = 8080;
 // Build app
 const app = express();
+app.use(express.json());
 
-const jsonData = JSON.parse(
+const authors = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
-
+// Read Data
 app.get('/api/v1/authors', (req, res) => {
   res.status(200).json({
     status: 'access',
-    results: jsonData.length,
+    results: authors.length,
     data: {
-      jsonData,
+      authors,
     },
   });
+});
+// Update Data
+app.post('/api/v1/authors', (req, res) => {
+  const newId = authors[authors.length - 1].id + 1;
+  const newAuthor = Object.assign({ id: newId }, req.body);
+  authors.push(newAuthor);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(authors),
+    (err) => {
+      res.status(201).json({
+        status: 'update',
+        data: {
+          authors,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {});
