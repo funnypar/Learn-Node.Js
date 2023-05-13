999affcefb27327cf1a607b8088a7aa0aa94646c
Mopano <99732517+funnypar@users.noreply.github.com>
2023-05-13 07:21:36 -0700

    addedDatabaseConnection

--- 011-authors/server.js
+++ 011-authors/server.js
@@ -1,4 +1,14 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 dotenv.config({ path: `${__dirname}/config.env` });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB).then(() => {
+  console.log('Your database connected successfully !!!');
+});
 const app = require('./app');
+
 app.listen(process.env.PORT, () => {});
