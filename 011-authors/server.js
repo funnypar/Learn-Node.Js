87bf111a732dfbf298b99348de399d6e741b8247
Mopano <99732517+funnypar@users.noreply.github.com>
2023-05-14 12:23:52 -0700

    addedDocToDatabase

--- 011-authors/server.js
+++ 011-authors/server.js
@@ -21,7 +21,7 @@ const authorScheme = new mongoose.Schema({
     required: [true, 'There is no name !!! '],
   },
   age: {
-    type: Boolean || Number,
+    type: Boolean && Number,
     default: false,
   },
   bookNumber: {
@@ -57,6 +57,33 @@ const authorScheme = new mongoose.Schema({
   },
 });
 const Author = mongoose.model('Author', authorScheme);
+
+const newAuthor = new Author({
+  id: 0,
+  name: 'Steve Toltz',
+  age: 51,
+  bookNumber: 3,
+  nationality: 'australia',
+  ratingsAverage: 7.4,
+  prize: [
+    '2008 Man Booker Prize',
+    '2008 Guardian First Book Award',
+    '2015 the Russell Prize',
+  ],
+  books: ['A Fraction of the Whole', 'Here Goes Nothing', 'Quicksand'],
+  description:
+    'Toltz attended Killara High School and graduated from the University of Newcastle, New South Wales, in 1994. Prior to his literary career, he lived in Montreal, Vancouver, New York City, Barcelona, and Paris, variously working as a cameraman, telemarketer, security guard, private investigator, English teacher, and screenwriter.Toltz married French-Australian artist and painter Marie Peter-Toltz in 2005. They have one son born in 2012. ',
+  imageCover: 'tour-1-cover.jpg',
+  images: ['tour-1-1.jpg', 'tour-1-2.jpg', 'tour-1-3.jpg'],
+});
+newAuthor
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error !!! ==> ', err);
+  });
 
 const app = require('./app');
 
