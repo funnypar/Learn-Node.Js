faf9e216705415ee22fabfa37f57984da2ad995c
Mopano <99732517+funnypar@users.noreply.github.com>
2023-05-14 00:38:09 -0700

    addedSchemeAndModel

--- 011-authors/server.js
+++ 011-authors/server.js
@@ -9,6 +9,55 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => {
   console.log('Your database connected successfully !!!');
 });
+const authorScheme = new mongoose.Schema({
+  id: {
+    type: Number,
+    unique: true,
+    required: [true, 'There is no Id number !!! '],
+  },
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'There is no name !!! '],
+  },
+  age: {
+    type: Boolean || Number,
+    default: false,
+  },
+  bookNumber: {
+    type: Number,
+    required: [true, 'There is no book number !!! '],
+  },
+  nationality: {
+    type: String,
+    required: [true, 'There is no nationality !!! '],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 5.5,
+  },
+  prize: {
+    type: Array || Boolean,
+  },
+  books: {
+    type: Array,
+    required: [true, 'There are no books !!! '],
+  },
+  description: {
+    type: String,
+    required: [true, 'There is no description !!! '],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'There is no cover image !!! '],
+  },
+  images: {
+    type: Array,
+    required: [true, 'There are no images !!! '],
+  },
+});
+const Author = mongoose.model('Author', authorScheme);
+
 const app = require('./app');
 
 app.listen(process.env.PORT, () => {});
